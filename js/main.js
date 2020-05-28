e0b22b0545621e32d334034b20d4dbb1842b82c8
Luciano Alcaraz <LGA-dev@hotmail.com>
2020-05-27 22:24:49 -0300

    Add deleteLastValue, deleteAllValues and squareRoot functions

--- js/main.js
+++ js/main.js
@@ -11,16 +11,45 @@ let inputsArray = [];
 let outputArray = 0;
 let operator = "";
 let calculationResult = 0;
+let squareRootResult = 0;
 
 // *  00. Global variables *
 
 
 
-// *  01. Array variables  *
+// *  01. Functions  *
 
 function storeValue(buttonValue){
     inputsArray.push(buttonValue);
     showOutput();
+    log();
+}
+
+function deleteLastValue(){
+    inputsArray.pop();
+    showOutput();
+    log();
+}
+
+function deleteAllValues(){
+    inputsArray = [];
+    outputArray = inputsArray;
+    document.getElementById('output-small').innerHTML = outputArray;
+    showOutput();
+    log();
+}
+
+function squareRoot() {
+    // output array is 25
+    squareRootResult = Math.sqrt(outputArray); // 5
+    // inputs array is [2,5]
+    // empty inputs array
+    inputsArray = [];
+    // push square root results into the emptied array
+    inputsArray.push(squareRootResult);
+
+    document.getElementById('output-normal').innerHTML = squareRootResult;
+    document.getElementById('output-small').innerHTML = `sqrt(${outputArray})`;
     log();
 }
 
@@ -43,8 +72,9 @@ function log() {
     console.log("Current value of outputArray: " + outputArray);
     console.log("Current value of inputsArray: " + inputsArray);
     console.log("Current value of Operator: " + operator);
+    console.log("Current value of squareRootResult: " + squareRootResult);
     console.log("Current value of calculationResult: " + calculationResult);
     console.log('----------------------------------------------');
 }
 
-// *  01. Array variables  *
+// *  01. Functions  *
