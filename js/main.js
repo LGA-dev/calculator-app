d595285e7cdd05fb488bc53ee9aaf8a3ac83fa8b
Luciano Alcaraz <LGA-dev@hotmail.com>
2020-06-01 21:18:28 -0300

    Code clean up

--- js/main.js
+++ js/main.js
@@ -8,10 +8,9 @@
 // *  00. Global variables *
 
 let inputsArray = [];
-let outputArray = 0;
-let operator = "";
+let outputValue = 0;
+let smallOutputValue = "";
 let calculationResult = 0;
-let squareRootResult = 0;
 
 // *  00. Global variables *
 
@@ -19,59 +18,64 @@ let squareRootResult = 0;
 
 // *  01. Functions  *
 
-function storeValue(buttonValue){
+function showOutput() {
+    document.getElementById('output-small').innerHTML = outputValue;
+    document.getElementById('output-normal').innerHTML = calculationResult;
+    log();
+}
+
+function storeValue(buttonValue) {
     inputsArray.push(buttonValue);
-    showOutput();
-    log();
-}
-
-function deleteLastValue(){
-    inputsArray.pop();
-    showOutput();
-    log();
-}
-
-function deleteAllValues(){
-    inputsArray = [];
-    outputArray = inputsArray;
-    document.getElementById('output-small').innerHTML = outputArray;
+    outputValue = inputsArray.join("");
+    calculateOne(outputValue);
     showOutput();
     log();
 }
 
 function squareRoot() {
-    squareRootResult = Math.sqrt(calculationResult);
-    inputsArray = [];
-    inputsArray.push(squareRootResult);
-
-    document.getElementById('output-normal').innerHTML = squareRootResult;
-    document.getElementById('output-small').innerHTML = `sqrt(${outputArray})`;
+    calculationResult = Math.sqrt(calculationResult);
+    inputsArray = []; // !
+    inputsArray.push(calculationResult);
+    showOutput();
     log();
 }
 
-function showOutput(){
-    outputArray = inputsArray.join("");
-    document.getElementById('output-normal').innerHTML = outputArray;
+function calculateOne(outputValue) {
+    calculationResult = eval(outputValue); //!
+    log();
+    return calculationResult;
+}
+
+function calculateAll() {
+    calculationResult = eval(outputValue); // !
+    outputValue = calculationResult; // !
+    smallOutputValue = "";
+    outputValue = "";
+    showOutput();
     log();
 }
 
-function calculateAll(){
-    outputArray = inputsArray.join("");
-    calculationResult = eval(outputArray);
+function deleteLastValue() {
+    inputsArray.pop();
+    showOutput();
+    log();
+}
+
+function deleteAllValues() {
     inputsArray = [];
-    document.getElementById('output-normal').innerHTML = calculationResult;
-    document.getElementById('output-small').innerHTML = outputArray;
+    outputValue = 0; // !
+    calculationResult = 0;
+    showOutput();
     log();
 }
 
 function log() {
-    console.log('----------------------------------------------');
-    console.log("Current value of outputArray: " + outputArray);
-    console.log("Current value of inputsArray: " + inputsArray);
-    console.log("Current value of Operator: " + operator);
-    console.log("Current value of squareRootResult: " + squareRootResult);
+    console.log('--------------------------------------------------');
+    console.log("Current value of inputsArray: "       + inputsArray);
+    console.log("Current value of outputValue: "       + outputValue);
+    console.log("Current value of smallOutputValue: "  + smallOutputValue);
     console.log("Current value of calculationResult: " + calculationResult);
-    console.log('----------------------------------------------');
+    console.log('--------------------------------------------------');
 }
 
 // *  01. Functions  *
